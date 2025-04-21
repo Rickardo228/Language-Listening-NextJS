d1ff2eb70f0cedbde07dd39381b3de3546973043
Ric Barnes <rickybarnes22@hotmail.com>
2025-04-21 09:20:36 +0100

    Streamlined playback settings for general user

--- src/app/configDefinitions.ts
+++ src/app/configDefinitions.ts
@@ -8,45 +8,45 @@ export type ConfigFieldDefinition = {
 };
 
 export const presentationConfigDefinition: ConfigFieldDefinition[] = [
-  { key: "bgImage", label: "Background Image", inputType: "file" },
+  // { key: "bgImage", label: "Background Image", inputType: "file" },
   {
     key: "containerBg",
-    label: "Container Background Color",
+    label: "Video Background Color",
     inputType: "color",
   },
   { key: "textBg", label: "Text Background Color", inputType: "color" },
-  { key: "enableSnow", label: "Enable Snow Effect", inputType: "checkbox" },
-  {
-    key: "enableCherryBlossom",
-    label: "Enable Cherry Blossom Effect",
-    inputType: "checkbox",
-  },
-  { key: "enableLeaves", label: "Enable Leaves Effect", inputType: "checkbox" },
-  {
-    key: "enableAutumnLeaves",
-    label: "Enable Autumn Leaves Effect",
-    inputType: "checkbox",
-  },
-  {
-    key: "enableOrtonEffect",
-    label: "Enable Orton Effect",
-    inputType: "checkbox",
-  },
-  {
-    key: "enableParticles",
-    label: "Enable Particle Effect",
-    inputType: "checkbox",
-  },
-  {
-    key: "enableSteam",
-    label: "Enable Steam Effect",
-    inputType: "checkbox",
-  },
-  {
-    key: "postProcessDelay",
-    label: "Delay After Processing (ms)",
-    inputType: "number",
-  },
+  // { key: "enableSnow", label: "Enable Snow Effect", inputType: "checkbox" },
+  // {
+  //   key: "enableCherryBlossom",
+  //   label: "Enable Cherry Blossom Effect",
+  //   inputType: "checkbox",
+  // },
+  // { key: "enableLeaves", label: "Enable Leaves Effect", inputType: "checkbox" },
+  // {
+  //   key: "enableAutumnLeaves",
+  //   label: "Enable Autumn Leaves Effect",
+  //   inputType: "checkbox",
+  // },
+  // {
+  //   key: "enableOrtonEffect",
+  //   label: "Enable Orton Effect",
+  //   inputType: "checkbox",
+  // },
+  // {
+  //   key: "enableParticles",
+  //   label: "Enable Particle Effect",
+  //   inputType: "checkbox",
+  // },
+  // {
+  //   key: "enableSteam",
+  //   label: "Enable Steam Effect",
+  //   inputType: "checkbox",
+  // },
+  // {
+  //   key: "postProcessDelay",
+  //   label: "Delay After Processing (ms)",
+  //   inputType: "number",
+  // },
   {
     key: "delayBetweenPhrases",
     label: "Delay Between Phrases (ms)",
