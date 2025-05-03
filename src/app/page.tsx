63e69cb7887f7ed10e4d8ad3b47a797beb9f9e0a
Ric Barnes <rickybarnes22@hotmail.com>
2025-05-03 20:04:14 +0100

    Add CollectionHeader component for managing collection title and actions

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,6 +14,7 @@ import { initializeApp } from 'firebase/app';
 import { getAuth, onAuthStateChanged, signInWithPopup, GoogleAuthProvider, User, signOut } from 'firebase/auth';
 import { getFirestore, collection, addDoc, getDocs, doc, updateDoc, deleteDoc } from 'firebase/firestore';
 import { CollectionList } from './CollectionList';
+import { CollectionHeader } from './CollectionHeader';
 
 const firebaseConfig = {
   apiKey: "AIzaSyBDe17ZSzrBpaae56p4YDpJ-2oXAV_89eg",
@@ -695,6 +696,18 @@ export default function Home() {
             )}
           </div>
         </div>
+
+        {/* Collection Title and Edit Buttons - shown on mobile when collection selected */}
+        {selectedCollection && (
+          <CollectionHeader
+            collectionId={selectedCollection}
+            savedCollections={savedCollections}
+            onRename={handleRenameCollection}
+            onDelete={handleDeleteCollection}
+            className="lg:hidden"
+            titleClassName="max-w-[150px]"
+          />
+        )}
       </div>
 
       {/* Audio Element */}
@@ -737,29 +750,14 @@ export default function Home() {
           {!loading && selectedCollection && (
             <div className={`sticky lg:px-0 lg:pb-3 px-1 py-2 top-[320px] lg:top-[0px] lg:bg-white bg-gray-50 z-1 ${!selectedCollection ? 'hidden lg:block' : ''}`}>
               <div className="w-full flex items-center p-2">
-                <div className="font-semibold truncate max-w-[250px]">
-                  {savedCollections.find(col => col.id === selectedCollection)?.name}
-                </div>
-                <div className="flex gap-2 p-2 pl-0">
-                  <button
-                    onClick={() => handleRenameCollection(selectedCollection)}
-                    className="p-1 text-gray-600 hover:text-blue-600"
-                    title="Rename"
-                  >
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-4 h-4">
-                      <path strokeLinecap="round" strokeLinejoin="round" d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10" />
-                    </svg>
-                  </button>
-                  <button
-                    onClick={() => handleDeleteCollection(selectedCollection)}
-                    className="p-1 text-gray-600 hover:text-red-600"
-                    title="Delete"
-                  >
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-4 h-4">
-                      <path strokeLinecap="round" strokeLinejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
-                    </svg>
-                  </button>
-                </div>
+                <CollectionHeader
+                  collectionId={selectedCollection}
+                  savedCollections={savedCollections}
+                  onRename={handleRenameCollection}
+                  onDelete={handleDeleteCollection}
+                  className="hidden lg:flex"
+                  titleClassName="max-w-[250px]"
+                />
                 <div className="w-fit whitespace-nowrap ml-auto">
                   <ImportPhrasesDialog
                     inputLang={inputLang}
