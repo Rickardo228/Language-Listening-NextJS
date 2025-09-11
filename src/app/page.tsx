a42702bf99142647a6bd4d0008d560c4eeda79e0
Ric Barnes <rickybarnes22@hotmail.com>
2025-09-11 22:45:53 +0100

    Update TemplatesBrowser titles and structure for improved user navigation
    - Change title of the first TemplatesBrowser to "Learn the Basics" and the second to "New Playlists"
    - Adjust the layout to include a max height and overflow handling for better content display
    - Maintain user preferences in the "Recommended for You" section, ensuring personalized content is still highlighted
    
    These updates enhance the clarity and usability of the TemplatesBrowser, providing a more intuitive experience for users.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,23 +7,22 @@ export default function Home() {
   const { userProfile } = useUser();
 
   return (
-    <div className="hidden lg:block p-3 flex-grow space-y-8">
+    <div className="hidden lg:block p-3 flex-grow space-y-8 max-h-full overflow-y-auto">
+      <TemplatesBrowser
+        pathId="beginner_path"
+        showHeader={false}
+        title="Learn the Basics"
+        className="mt-4"
+        noTemplatesComponent={<></>}
+      />
       <TemplatesBrowser
         showHeader={false}
-        title="General Templates"
+        title="New Playlists"
       />
       <TemplatesBrowser
         showHeader={false}
         title="Recommended for You"
         tags={userProfile?.contentPreferences || []}
-        className="mt-8"
-        noTemplatesComponent={<></>}
-      />
-      <TemplatesBrowser
-        pathId="beginner_path"
-        showHeader={false}
-        title="Beginner Learning Path"
-        className="mt-8"
         noTemplatesComponent={<></>}
       />
     </div>
